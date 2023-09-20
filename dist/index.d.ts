538a1727df397d7f3a6aac9bc8d99cc1c3a02d0f
zhwir <zhwir@163.com>
2023-09-20 17:01:52 +0800

    add js

---
+++ dist/index.d.ts
@@ -0,0 +1,25 @@
+import { ConnectorUpdate } from '@web3-react-wan/types';
+import { AbstractConnector } from '@web3-react-wan/abstract-connector';
+import Web3ProviderEngine from 'web3-provider-engine';
+interface WanWalletConnectorArguments {
+    chainId: number;
+    url: string;
+    pollingInterval?: number;
+    requestTimeoutMs?: number;
+}
+export declare class WanWalletConnector extends AbstractConnector {
+    private readonly chainId;
+    private readonly url;
+    private readonly pollingInterval?;
+    private readonly requestTimeoutMs?;
+    private provider;
+    constructor({ chainId, url, pollingInterval, requestTimeoutMs, }: WanWalletConnectorArguments);
+    activate(): Promise<ConnectorUpdate>;
+    private handleDisconnect;
+    getProvider(): Promise<Web3ProviderEngine>;
+    getChainId(): Promise<number>;
+    getAccount(): Promise<null>;
+    deactivate(): void;
+    close(): Promise<void>;
+}
+export {};
